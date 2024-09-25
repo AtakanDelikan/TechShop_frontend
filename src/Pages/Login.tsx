df8eaafd626dbfb4e28a5291fb1bc888ea2fe2fc
AtakanDelikan <atakan798@gmail.com>
2024-09-25 03:15:26 +0200

    login page added

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { inputHelper } from "../Helper";
+import { useLoginUserMutation } from "../Apis/authApi";
+import { apiResponse } from "../Interfaces";
 
 function Login() {
+  const [error, setError] = useState("");
+  const [loginUser] = useLoginUserMutation();
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const handleUserInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempData = inputHelper(e, userInput);
+    setUserInput(tempData);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    const response: apiResponse = await loginUser({
+      userName: userInput.userName,
+      password: userInput.password,
+    });
+    if (response.data) {
+      console.log(response.data);
+    } else if (response.error) {
+      console.log(response.error.data.errorMessages[0]);
+      setError(response.error.data.errorMessages[0]);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="container text-center">
-      <form className="mb-5" method="post">
+      <form className="mb-5" method="post" onSubmit={handleSubmit}>
         <h1 className="mt-5">Login</h1>
         <div className="mt-5">
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -13,6 +48,9 @@ function Login() {
               className="form-control"
               placeholder="Enter Username"
               required
+              name="userName"
+              value={`${userInput.userName}`}
+              onChange={handleUserInput}
             />
           </div>
 
@@ -22,11 +60,15 @@ function Login() {
               className="form-control"
               placeholder="Enter Password"
               required
+              name="password"
+              value={`${userInput.password}`}
+              onChange={handleUserInput}
             />
           </div>
         </div>
 
         <div className="mt-2">
+          {error && <p className="text-danger">{error}</p>}
           <button
             type="submit"
             className="btn btn-success"
