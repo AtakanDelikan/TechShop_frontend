5b2a19398216dc81756219f10d8581228cfb264a
AtakanDelikan <atakan798@gmail.com>
2025-03-17 02:23:51 +0100

    refactored PageSellector

--- src/Components/Page/Common/PageSellector.tsx
+++ src/Components/Page/Common/PageSellector.tsx
@@ -7,9 +7,31 @@ interface Props {
 }
 
 function PageSelector(props: Props) {
+  const pageNumbers = [];
+
+  if (props.totalPages <= 10) {
+    // Show all pages if total is small
+    for (let i = 1; i <= props.totalPages; i++) {
+      pageNumbers.push(i);
+    }
+  } else {
+    // Always include first & last page
+    pageNumbers.push(1);
+
+    let start = Math.max(2, props.currentPage - 2);
+    let end = Math.min(props.totalPages - 1, props.currentPage + 2);
+
+    if (start > 2) pageNumbers.push("...");
+    for (let i = start; i <= end; i++) pageNumbers.push(i);
+    if (end < props.totalPages - 1) pageNumbers.push("...");
+
+    pageNumbers.push(props.totalPages);
+  }
+
   return (
     <nav className="pb-2">
       <ul className="pagination justify-content-center my-3">
+        {/* Previous Button */}
         <li
           className={`page-item ${props.currentPage === 1 ? "disabled" : ""}`}
         >
@@ -20,21 +42,29 @@ function PageSelector(props: Props) {
             Previous
           </button>
         </li>
-        {[...Array(props.totalPages)].map((_, i) => (
+
+        {/* Page Numbers */}
+        {pageNumbers.map((num, index) => (
           <li
-            key={i + 1}
-            className={`page-item ${
-              props.currentPage === i + 1 ? "active" : ""
-            }`}
+            key={index}
+            className={`page-item ${num === props.currentPage ? "active" : ""}`}
           >
-            <button
-              className="page-link"
-              onClick={() => props.onPageSelect(i + 1)}
-            >
-              {i + 1}
-            </button>
+            {num === "..." ? (
+              <span className="page-link">...</span>
+            ) : (
+              <button
+                className="page-link"
+                onClick={() =>
+                  props.onPageSelect(parseInt(num.toString()) || 1)
+                }
+              >
+                {num}
+              </button>
+            )}
           </li>
         ))}
+
+        {/* Next Button */}
         <li
           className={`page-item ${
             props.currentPage === props.totalPages ? "disabled" : ""
